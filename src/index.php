b362be4599792b1808924319dca2e3b0022bcebc
Kamesh Chanuka <chanukabdk@gmail.com>
2026-01-07 22:05:50 +0530

    Added dashboard Dashboard added with navigation links to other pages. Dashboard support classic Broken Access Control vulnerabilities

--- src/index.php
+++ src/index.php
@@ -1,2 +1,19 @@
-<?php
-echo "Hello from vulnerable PHP lab";
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Vulnerable PHP App</title>
+    </head>
+    <body>
+
+        <h1>Vulnerable PHP OWASP Top 10 Lab</h1>
+
+        <p>Welcome to the intentionally vulnerable PHP application.</p>
+
+        <ul>
+            <li><a href="auth/register.php">Register</a></li>
+            <li><a href="auth/login.php">Login</a></li>
+            <li><a href="dashboard.php">Dashboard</a></li>
+        </ul>
+
+    </body>
+</html>
